d3eb137ed01aafcf86da099497caaf564814c77c
Mihaita <124569822+mhingan@users.noreply.github.com>
2023-09-22 18:53:35 +0300

    Initial commit

---
+++ src/test/java/com/example/finalprojectscit/FinalProjectScitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectscit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectScitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
