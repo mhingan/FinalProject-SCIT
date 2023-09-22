54caf37223ffe1e37d35ea867b985a9502f60af7
Mihaita <124569822+mhingan@users.noreply.github.com>
2023-09-22 19:38:30 +0300

    post/user-services: - add find all / find by id - 200 OK

--- src/main/java/com/example/finalprojectscit/service/PostService.java
+++ src/main/java/com/example/finalprojectscit/service/PostService.java
@@ -1,4 +1,30 @@
 package com.example.finalprojectscit.service;
 
+import com.example.finalprojectscit.model.Post;
+import com.example.finalprojectscit.repository.PostRepository;
+import com.example.finalprojectscit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findById(int id) {
+        return postRepository.findById(id).orElseThrow(() -> new RuntimeException("No post found with id: " + id));
+    }
 }
--- src/main/java/com/example/finalprojectscit/service/UserService.java
+++ src/main/java/com/example/finalprojectscit/service/UserService.java
@@ -1,4 +1,29 @@
 package com.example.finalprojectscit.service;
 
+import com.example.finalprojectscit.repository.PostRepository;
+import com.example.finalprojectscit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(int id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("No user found with id: " + id));
+    }
 }
